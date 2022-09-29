dd49cb97477585c9cb3190c7e204d9c31a9f635f
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-09-29 03:59:29 -0300

    impl-Rep

---
+++ src/main/java/com/Deusley/bookservice/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package com.Deusley.bookservice.repository;
+
+import com.Deusley.bookservice.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository <Book, Long> { }
